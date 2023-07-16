0180808e12b2d2feb1c341d5c97aba3c608b31d2
twave <wjdrldyd0213@gmail.com>
2023-07-16 21:54:59 +0900

    header_rightside1(만드는 과정)

---
+++ component/sign_in.tsx
@@ -0,0 +1,8 @@
+import header_box from './header_box.module.css'
+
+export default function Sign_in_box() {
+    return <div className={header_box.sign_in}>
+        <button type="button" className={header_box.button}>Sign in</button>
+    </div>;
+  }
+  
--- pages/examplepage.tsx
+++ pages/examplepage.tsx
@@ -1,4 +1,19 @@
-// 이것의 화면을 보고 싶다면 http://localhost:3000/examplepage로 들어가세요
+// 이것의 화면을 보고 싶다면 http://localhost:3000/examplepage 로 들어가세요
+
+import Header_search_box from "../component/search_box"
+import Sign_in_box from "../component/sign_in";
+import header from "../component/header.module.css"
+
 export default function index() {
-  return <div>이것은 예시입니다.</div>;
+  return <div className={header.rightside}>
+    <Header_search_box />
+    <Sign_in_box />
+  </div>;
+  // <div className={header.header}>
+  //   <div className={header.leftside}>
+  //     <img src="45logo_white.png"></img>
+  //   </div>
+    
+  // </div>;
+    
 }
