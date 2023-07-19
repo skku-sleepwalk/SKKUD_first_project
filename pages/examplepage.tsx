1c37dea467d27b97b56483b2640f44406a8cbd81
Twave1717 <wjdrldyd0213@gmail.com>
2023-07-20 00:33:43 +0900

    orderBtn 완성

--- pages/examplepage.tsx
+++ pages/examplepage.tsx
@@ -1,4 +1,8 @@
 // 이것의 화면을 보고 싶다면 http://localhost:3000/examplepage로 들어가세요
+
+import {DoubleBtn} from '@/component/DoubleBtn'
+
 export default function index() {
-  return <div>이것은 예시입니다.</div>;
+
+  return <DoubleBtn value1='인원 많은 순' value2='최신 순' />;
 }
