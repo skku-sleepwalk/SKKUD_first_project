c21fd5cf069fd461b6cb1ed121cdd99917c4c477
twave <wjdrldyd0213@gmail.com>
2023-07-24 09:29:01 +0900

    ListBlock map 사용 update

--- pages/listPage.tsx
+++ pages/listPage.tsx
@@ -3,49 +3,54 @@ import { SearchedResult } from '@/component/searchedText/SearchedResult';
 import { DoubleBtn } from '@/component/DoubleBtn/DoubleBtn'
 import { ListBlock } from '../component/listsBlock/listBlock';
 
-const lectureInfoList = [
-    {
-        'lectureName': '프로그래밍 기초와 실습',
-        'instructorName': '김영훈',
-        'paragraph': '프로그래밍 기초와 실습을 함께 공부할 분들을 찾습니다!',
-        'memberCnt': '5',
-        'members': [
-            "S1", "S2", "S3", "S4", "S5"
-        ]
-    },
-    {
-        'lectureName': '프로그래밍 기초와 실습',
-        'instructorName': '황성재',
-        'paragraph': '1일 1솔 챌린지',
-        'memberCnt': '2',
-        'members': [
-            "S1", "S2"
-        ]
-    },
-    {
-        'lectureName': '웹프로그래밍 실습',
-        'instructorName': '이이익',
-        'paragraph': 'FrontEndProgramming && BackEndProgramming',
-        'memberCnt': '1',
-        'members': [
-            "S1"
-        ]
-    }
-]
-
 export default function index() {
     
+    const lectureInfoList = [
+        {
+            'lectureName': '프로그래밍 기초와 실습',
+            'instructorName': '김영훈',
+            'paragraph': '프로그래밍 기초와 실습을 함께 공부할 분들을 찾습니다!',
+            'memberCnt': '5',
+            'members': [
+                "S1", "S2", "S3", "S4", "S5"
+            ]
+        },
+        {
+            'lectureName': '프로그래밍 기초와 실습',
+            'instructorName': '황성재',
+            'paragraph': '1일 1솔 챌린지',
+            'memberCnt': '2',
+            'members': [
+                "S1", "S2"
+            ]
+        },
+        {
+            'lectureName': '웹프로그래밍 실습',
+            'instructorName': '이이익',
+            'paragraph': 'FrontEndProgramming && BackEndProgramming',
+            'memberCnt': '1',
+            'members': [
+                "S1"
+            ]
+        }
+    ]
+
     return <div>
         <Header />
         <SearchedResult query="프기실" ResultCnt={3} />
         <div>
             <DoubleBtn value1='인원 많은 순' value2='최신 순' />
-            {lectureInfoList.map((lectureInfo) => {
-                return lectureInfo;
-            })}
-            <ListBlock lectureName="프로그래밍 기초와 실습" instructorName="김영훈" paragraph="설명글1" memberCnt={5} members={["S1", "S2", "S3", "S4", "S5"]} />
-            <ListBlock lectureName="프로그래밍 기초와 실습" instructorName="황성재" paragraph="설명글2" memberCnt={2} members={["S1", "S2"]} />
-            <ListBlock lectureName="웹프로그래밍 실습" instructorName="크롱" paragraph="크롱!" memberCnt={1} members={["S1", "S3"]} />
+            {
+                lectureInfoList.map((lectureInfo) => {
+                    return <ListBlock 
+                        lectureName={lectureInfo['lectureName']}
+                        instructorName={lectureInfo['instructorName']}
+                        paragraph={lectureInfo['paragraph']}
+                        memberCnt={lectureInfo['memberCnt']}
+                        members={lectureInfo['members']}
+                    />;
+                })
+            }
         </div>
     </div>;
   }
