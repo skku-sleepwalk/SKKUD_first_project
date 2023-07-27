aba28fdcda2ebbd492a9dedb79d19b91a4f1f380
Twave1717 <wjdrldyd0213@gmail.com>
2023-07-27 18:50:21 +0900

    branch made

---
+++ pages/login.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/header/header'
+
+
+export default function index() {
+    return <div>
+        <Header />
+        <LoginHeader />
+        <LoginForm />
+    </div>;
+  }
+  
