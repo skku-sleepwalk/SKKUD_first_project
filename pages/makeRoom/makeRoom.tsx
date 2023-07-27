339281e546843c7bdf5a77097f5e5bb1754cb6e8
skku-sleepwalk <lucas7211@g.skku.edu>
2023-07-27 22:23:27 +0900

    대충 요일처리 할 수 있을 듯

--- pages/makeRoom/makeRoom.tsx
+++ pages/makeRoom/makeRoom.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import styles from "./makeRoom.module.css";
 export default function main() {
+  const [week, setweek] = useState(0);
   return (
     <div className={styles.backgroundColor}>
       <style jsx global>
@@ -12,13 +14,52 @@ export default function main() {
       </style>
       <div
         style={{
-          height: "60%",
+          height: "69%",
           width: "45%",
           backgroundColor: "white",
           margin: "20vh",
-          marginLeft: "55vh",
+          marginLeft: "58vh",
         }}
-      ></div>
+      >
+        <div style={{ marginLeft: "2rem", paddingTop: "1.5rem" }}>
+          <h1 className={styles.h1}>방 만들기</h1>
+        </div>
+        <div
+          style={{
+            // border: "solid black 0.5rem",
+            margin: "1rem",
+            marginTop: "1.5rem",
+            width: "95%",
+            height: "75%",
+          }}
+        >
+          {/* 버튼 포함 */}
+          <div
+            style={{
+              border: "solid black 0.05rem",
+              width: "100%",
+              height: "90%",
+              borderRadius: "0.5rem",
+            }}
+          >
+            {week == 0 ? (
+              <button
+                style={{
+                  height: "50px",
+                  width: "50px",
+                }}
+              ></button>
+            ) : (
+              <button
+                style={{
+                  height: "50px",
+                  width: "50px",
+                }}
+              ></button>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
