d826aa37731b5daaad906c549b3e54b88b5cda15
skku-sleepwalk <lucas7211@g.skku.edu>
2023-07-31 20:02:23 +0900

    일단 내부 버튼 7개 완성

--- pages/makeRoom/makeRoom.tsx
+++ pages/makeRoom/makeRoom.tsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 import styles from "./makeRoom.module.css";
 export default function main() {
-  const [week, setweek] = useState(0);
+  const [week1, setweek1] = useState(0);
+  const [week2, setweek2] = useState(0); //이걸로 날짜 보내면 될듯
+  const [week3, setweek3] = useState(0);
+  const [week4, setweek4] = useState(0);
+  const [week5, setweek5] = useState(0);
+  const [week6, setweek6] = useState(0);
+  const [week7, setweek7] = useState(0);
+  //이거 배열로바꿔야... 여러개 선택 가능해야 함
+  //아니면 걍 여러개도 나쁘지않은듯
   return (
     <div className={styles.backgroundColor}>
       <style jsx global>
@@ -12,27 +20,11 @@ export default function main() {
           }
         `}
       </style>
-      <div
-        style={{
-          height: "69%",
-          width: "45%",
-          backgroundColor: "white",
-          margin: "20vh",
-          marginLeft: "58vh",
-        }}
-      >
+      <div className={styles.frame}>
         <div style={{ marginLeft: "2rem", paddingTop: "1.5rem" }}>
           <h1 className={styles.h1}>방 만들기</h1>
         </div>
-        <div
-          style={{
-            // border: "solid black 0.5rem",
-            margin: "1rem",
-            marginTop: "1.5rem",
-            width: "95%",
-            height: "75%",
-          }}
-        >
+        <div className={styles.innerframe}>
           {/* 버튼 포함 */}
           <div
             style={{
@@ -43,7 +35,7 @@ export default function main() {
               paddingTop: "3vh",
             }}
           >
-            {week == 0 ? (
+            {week1 == 1 ? (
               <button
                 style={{
                   height: "50px",
@@ -54,6 +46,9 @@ export default function main() {
                   color: "white",
                   fontSize: "25px",
                   fontWeight: "bold",
+                }}
+                onClick={() => {
+                  setweek1(0);
                 }}
               >
                 월
@@ -71,13 +66,13 @@ export default function main() {
                   fontWeight: "bold",
                 }}
                 onClick={() => {
-                  setweek(0);
+                  setweek1(1);
                 }}
               >
                 월
               </button>
             )}
-            {week == 1 ? (
+            {week2 == 1 ? (
               <button
                 style={{
                   height: "50px",
@@ -88,6 +83,9 @@ export default function main() {
                   color: "white",
                   fontSize: "25px",
                   fontWeight: "bold",
+                }}
+                onClick={() => {
+                  setweek2(0);
                 }}
               >
                 화
@@ -105,10 +103,197 @@ export default function main() {
                   fontWeight: "bold",
                 }}
                 onClick={() => {
-                  setweek(1);
+                  setweek2(1);
                 }}
               >
                 화
+              </button>
+            )}
+
+            {week3 == 1 ? (
+              <button
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  borderRadius: "0",
+                  border: "0",
+                  backgroundColor: "#246633",
+                  color: "white",
+                  fontSize: "25px",
+                  fontWeight: "bold",
+                }}
+                onClick={() => {
+                  setweek3(0);
+                }}
+              >
+                수
+              </button>
+            ) : (
+              <button
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  borderRadius: "0",
+                  border: "0",
+                  backgroundColor: "#F2F2F2",
+                  color: "#B2B2B2",
+                  fontSize: "25px",
+                  fontWeight: "bold",
+                }}
+                onClick={() => {
+                  setweek3(1);
+                }}
+              >
+                수
+              </button>
+            )}
+
+            {week4 == 1 ? (
+              <button
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  borderRadius: "0",
+                  border: "0",
+                  backgroundColor: "#246633",
+                  color: "white",
+                  fontSize: "25px",
+                  fontWeight: "bold",
+                }}
+                onClick={() => {
+                  setweek4(0);
+                }}
+              >
+                목
+              </button>
+            ) : (
+              <button
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  borderRadius: "0",
+                  border: "0",
+                  backgroundColor: "#F2F2F2",
+                  color: "#B2B2B2",
+                  fontSize: "25px",
+                  fontWeight: "bold",
+                }}
+                onClick={() => {
+                  setweek4(1);
+                }}
+              >
+                목
+              </button>
+            )}
+            {week5 == 1 ? (
+              <button
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  borderRadius: "0",
+                  border: "0",
+                  backgroundColor: "#246633",
+                  color: "white",
+                  fontSize: "25px",
+                  fontWeight: "bold",
+                }}
+                onClick={() => {
+                  setweek5(0);
+                }}
+              >
+                금
+              </button>
+            ) : (
+              <button
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  borderRadius: "0",
+                  border: "0",
+                  backgroundColor: "#F2F2F2",
+                  color: "#B2B2B2",
+                  fontSize: "25px",
+                  fontWeight: "bold",
+                }}
+                onClick={() => {
+                  setweek5(1);
+                }}
+              >
+                금
+              </button>
+            )}
+            {week6 == 1 ? (
+              <button
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  borderRadius: "0",
+                  border: "0",
+                  backgroundColor: "#246633",
+                  color: "white",
+                  fontSize: "25px",
+                  fontWeight: "bold",
+                }}
+                onClick={() => {
+                  setweek6(0);
+                }}
+              >
+                토
+              </button>
+            ) : (
+              <button
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  borderRadius: "0",
+                  border: "0",
+                  backgroundColor: "#F2F2F2",
+                  color: "#B2B2B2",
+                  fontSize: "25px",
+                  fontWeight: "bold",
+                }}
+                onClick={() => {
+                  setweek6(1);
+                }}
+              >
+                토
+              </button>
+            )}
+            {week7 == 1 ? (
+              <button
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  borderRadius: "0",
+                  border: "0",
+                  backgroundColor: "#246633",
+                  color: "white",
+                  fontSize: "25px",
+                  fontWeight: "bold",
+                }}
+                onClick={() => {
+                  setweek7(0);
+                }}
+              >
+                일
+              </button>
+            ) : (
+              <button
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  borderRadius: "0",
+                  border: "0",
+                  backgroundColor: "#F2F2F2",
+                  color: "#B2B2B2",
+                  fontSize: "25px",
+                  fontWeight: "bold",
+                }}
+                onClick={() => {
+                  setweek7(1);
+                }}
+              >
+                일
               </button>
             )}
           </div>
