f81b12819fab5b380411b34fca6587799d8c4046
skku-sleepwalk <lucas7211@g.skku.edu>
2023-08-22 19:15:51 +0900

    8080

--- utils/makeRoom.ts
+++ utils/makeRoom.ts
@@ -4,7 +4,7 @@ import { Room, makeRoomResponse } from "@/types/Rooms";
 
 export async function MakeRoom(post: Room): Promise<makeRoomResponse> {
   const { data } = await axios.post<Room, AxiosResponse<makeRoomResponse>>(
-    `/board`,
+    `http://localhost:8080/board`,
     post,
     {
       headers: {},
