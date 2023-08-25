5dbc1dc7c94d150b12358188ada1e07c3d09d16a
BanYeah <126534852+BanYeah@users.noreply.github.com>
2023-08-25 18:31:08 +0900

    메인페이지와 검색 연결

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
 import styles from "./SearchBar.module.css";
+import { useRouter } from "next/router";
 export function SearchBar() {
+  const router = useRouter();
+  const [keyword, setKeyword] = useState<string>("");
+
+  // 엔터 키 누를 시 submit 발생
+  const handleKeyDown = (e: any) => {
+    if (e.key === "Enter") {
+      e.preventDefault(); // 기본 엔터 키 동작 막기
+      setKeyword("");
+      keyword === ""
+        ? router.replace(`/study`)
+        : router.replace(`/study?search=${keyword}`);
+    }
+  };
+
   return (
     <div className={styles.search} style={{ marginLeft: "25%" }}>
       <img
@@ -8,7 +24,14 @@ export function SearchBar() {
         height="20"
         style={{ position: "absolute", marginTop: "15px", marginLeft: "15px" }}
       ></img>
-      <input type="text" id={styles.Search} placeholder="검색" />
+      <input
+        type="text"
+        id={styles.Search}
+        placeholder="검색"
+        value={keyword}
+        onChange={(e) => setKeyword(e.currentTarget.value)}
+        onKeyDown={handleKeyDown}
+      />
     </div>
   );
 }
